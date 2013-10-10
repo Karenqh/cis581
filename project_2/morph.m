b4c604fa9c1d02d9bd45bc771e43657cc6edc5bf
Qin He <heqin@seas.upenn.edu>
2013-10-10 02:36:40 -0400

    No need to flip ys, since cpselect output in image coordinates

--- project_2/morph.m
+++ project_2/morph.m
@@ -4,13 +4,10 @@ function morphed_im = morph(im1, im2, im1_pts, im2_pts, tri, warp_frac, dissolve
 nr = size(im1, 1);
 nc = size(im1, 2);
 inds = 1:(nr*nc);
-% [cols, rows] = meshgrid(1:nc, 1:nr);
 
 
 % Generate coordinates
 [I J] = ind2sub([nr, nc], inds);
-% Convert to x-y coordinates
-I = fliplr(I); % y
 % Convert to column vectors
 I = I'; J = J';
 
@@ -25,31 +22,6 @@ idx = pointLocation(dt_tmp, J, I);
 
 dt = dt_tmp.Triangulation;
 dt_size = size(dt,1);
-
-% % Compute the coordinates of vertices of triangluars
-% if tri_done == false
-% 
-%     % Source image
-%     src_tri = cell(dt_size,1);
-%     for i = 1:dt_size
-%         pt1 = im1_pts(dt(i,1), :);
-%         pt2 = im1_pts(dt(i,2), :);
-%         pt3 = im1_pts(dt(i,3), :);
-%         src_tri{i} = [pt1; pt2; pt3];   
-%     end
-%     
-%     % Target image
-%     tar_tri = cell(dt_size,1);
-%     for i = 1:dt_size
-%         pt1 = im2_pts(dt(i,1), :);
-%         pt2 = im2_pts(dt(i,2), :);
-%         pt3 = im2_pts(dt(i,3), :);
-%         tar_tri{i} = [pt1; pt2; pt3];
-%     end
-%     
-%     tri_done = true;
-% end
-
 
 % Compute the barycentric coordinate
 tmp_im1 = uint8( zeros(size(im1)) );
@@ -67,14 +39,13 @@ for tri_i = 1:dt_size
     xs = J(idx==tri_i);  % col
     ys = I(idx==tri_i);  % col
     % Convert xy to ij
-    ys_converted = nr - (ys-1);
-    inter_ind = sub2ind([nr nc], ys_converted, xs);
+    inter_ind = sub2ind([nr nc], ys, xs);
     
     % Barycentric coordinates
     bar_coor = inter_tri \ [xs'; ys'; ones(1, length(xs))];
         
-    %%% Process pixels from SOURCE image
     
+    %%% Process pixels from SOURCE image
     pt1 = im1_pts(dt(tri_i,1), :);
     pt2 = im1_pts(dt(tri_i,2), :);
     pt3 = im1_pts(dt(tri_i,3), :);
@@ -82,8 +53,7 @@ for tri_i = 1:dt_size
     src_pixel = [src_tri_verts'; 1 1 1] * bar_coor;  % 3-by-N
     
     % Convert to row/col and Round
-    src_pixel(2,:) = floor( I(1) -  src_pixel(2,:) );
-    src_pixel(1,:) = floor(src_pixel(1,:));
+    src_pixel = floor(src_pixel);
     xss = src_pixel(1,:);
     yss = src_pixel(2,:);
     xss(xss<=0) = 1;    xss(xss>nc) = nc;
@@ -105,13 +75,9 @@ for tri_i = 1:dt_size
     pt3 = im2_pts(dt(tri_i,3), :);
     tar_tri_verts = [pt1; pt2; pt3];
     tar_pixel = [tar_tri_verts'; 1 1 1] * bar_coor;  % 3-by-N
-    % Homogeneous coordinates
-%     src_pixel = bsxfun(@rdivide, src_pixel, src_pixel(3,:));
-    % Assume all elements in 3rd row are ones
     
     % Convert to row/col and Round
-    tar_pixel(2,:) = floor( I(1) -  tar_pixel(2,:) );
-    tar_pixel(1,:) = floor(tar_pixel(1,:));
+    tar_pixel = floor(tar_pixel);
     xss = tar_pixel(1,:);
     yss = tar_pixel(2,:);
     xss(xss<=0) = 1;    xss(xss>nc) = nc;
@@ -130,9 +96,4 @@ end
 
 % CROSS DISSOLVE
 morphed_im = tmp_im1*(1-dissolve_frac) + tmp_im2*dissolve_frac;
-
-
-% INVERSE WARPING
-
-
 
